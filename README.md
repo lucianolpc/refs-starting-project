756a04b2bd5263740caee4651c22325dbf40cc86
lucianolpc <luciano.junior@dcomp.ufs.br>
2025-09-04 23:28:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto de Prática com Refs
+
+Este código é referente ao quarto projeto do Curso:
+
+**[React - The Complete Guide 2025 (incl. Next.js, Redux)](https://www.udemy.com/course/react-the-complete-guide-incl-redux/)**
+
+Iniciado na Seção 8 e lecionado por:
+
+**[Academind by Maximilian Schwarzmüller](https://www.udemy.com/user/academind/)**
+
+Na plataforma de ensino online **[Udemy](https://www.udemy.com/)**.
+
+## Lista de aulas realizadas deste projeto:
+
+-
---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact, { rules } from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // Desabilita a exigência de React no escopo para JSX
+      "react/react-in-jsx-scope": "off",
+      // Desabilita a exigência de declaração de tipos de props
+      "react/prop-types": "off",
+      // ...outras regras...
+    },
+  },
+]);
