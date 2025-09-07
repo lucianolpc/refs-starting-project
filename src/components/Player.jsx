9468b131689382e5143c6a9e8987c1803c8ff02d
lucianolpc <luciano.junior@dcomp.ufs.br>
2025-09-07 16:52:45 -0300

    Criação dos cards de desafio e diálogos usando ref.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,11 +3,11 @@ import { useState, useRef } from "react";
 export default function Player() {
   const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
-    playerName.current.value = '';
+    playerName.current.value = "";
   }
 
   return (
